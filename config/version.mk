4bc1f885e86a6e9dca0c34280bd9a298e9aa82b8
Pavel Dubrova <pashadubrova@gmail.com>
2018-12-19 18:18:18 +0200

    Change Rise OS Build ID format
    Builds are identified with the build ID format P.YYMMDD.bbb[.rN], where:
      - P - represents the first letter of the code name of the platform
        release, e.g. P is Pie.
      - YYMMDD - identifies the date when the release is branched from or
        synced with the development branch. It is not guaranteed to be the
        exact date at which a build was made as it is common for minor
        variations added to an existing build to re-use the same date code
        as the existing build.
      - bbb - identifies individual versions related to the same date code,
        sequentially starting with 001.
      - rN - is an optional, alphanumeric that identifies the currently used
        source code tag.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -18,12 +18,14 @@ VENDOR := RiseOS
 # OS version
 ROM_VERSION := 9.0.0
 
-# Build ID format: a.b.c_rN
-# a - major number
-# b - minor number
-# c - build number
-# rN - revision number
-BUILD_ID := 1.0.0_r21
+# Builds are identified with the build ID format P.YYMMDD.bbb[.rN], where:
+# P - represents the first letter of the code name of the platform release, e.g. P is Pie.
+# YYMMDD - identifies the date when the release is branched from or synced with the development
+# branch. It is not guaranteed to be the exact date at which a build was made as it is common
+# for minor variations added to an existing build to re-use the same date code as the existing build.
+# bbb - identifies individual versions related to the same date code, sequentially starting with 001.
+# rN - is an optional, alphanumeric that identifies the currently used source code tag.
+BUILD_ID := P.181205.006.r21
 
 # ROM properties
 PRODUCT_PROPERTY_OVERRIDES += \
